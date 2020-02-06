0af8d39305b53c558228bc7274259161c5979f1f
demarcus kennedy <demarcuskennedy95@gmail.com>
2020-02-06 11:42:02 -0600

    Updates

---
+++ precache-manifest.17dfbffd9013a054e4d12ad9ed7a6422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af55bbc2adfbb9cba421ec425f0ae1f8",
+    "url": "/dkennedy881.github.io/fridgeryList/index.html"
+  },
+  {
+    "revision": "2110bab5d2898d95df75",
+    "url": "/dkennedy881.github.io/fridgeryList/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1c563676c82a86a1fd37",
+    "url": "/dkennedy881.github.io/fridgeryList/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2110bab5d2898d95df75",
+    "url": "/dkennedy881.github.io/fridgeryList/static/js/2.8685c8e3.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/dkennedy881.github.io/fridgeryList/static/js/2.8685c8e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c563676c82a86a1fd37",
+    "url": "/dkennedy881.github.io/fridgeryList/static/js/main.4e9f1431.chunk.js"
+  },
+  {
+    "revision": "ef164d670c0d02f0ba28",
+    "url": "/dkennedy881.github.io/fridgeryList/static/js/runtime-main.4cbe0272.js"
+  }
+]);
